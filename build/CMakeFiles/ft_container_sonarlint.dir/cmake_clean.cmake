098e2e577d1668b94e06fa847b3f8329ca57b54a
Yongman Kim <yongmkim@c1r1s3.42seoul.kr>
2022-11-15 17:18:51 +0900

    [sonarlint] add cmake, sonarlint config file

---
+++ build/CMakeFiles/ft_container_sonarlint.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ft_container_sonarlint.dir/main.cpp.o"
+  "CMakeFiles/ft_container_sonarlint.dir/main.cpp.o.d"
+  "ft_container_sonarlint"
+  "ft_container_sonarlint.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ft_container_sonarlint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /goinfre/yongmkim/ft_containers
+# Build directory: /goinfre/yongmkim/ft_containers/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
