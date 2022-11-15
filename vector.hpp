56013e40adac6a8115cbda1af0e12a8193ece631
Yongman Kim <yongmkim@c1r1s3.42seoul.kr>
2022-11-15 17:22:57 +0900

    [edit] init template in vector.hpp

---
+++ vector.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yongmkim <yongmkim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 17:21:18 by yongmkim          #+#    #+#             */
+/*   Updated: 2022/11/15 17:22:41 by yongmkim         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#ifndef __VECTOR_HPP__
+# define __VECTOR_HPP__
+
+# include <string>
+
+namespace ft	{
+
+	class vector {
+
+
+	};
+
+}; /* namespace ft */
+
+#endif /* __VECTOR_HPP__ */
